45bf8276b6a7ac4ed8dbeb3412a1b99cff034d48
JeremyLetang <letang.jeremy@gmail.com>
2013-11-12 14:30:56 -0500

    make test main compile on current rust master

--- src/test/main.rs
+++ src/test/main.rs
@@ -1,40 +1,40 @@
-extern mod portaudio;
+#[feature(globs)];
 
-use std::io;
+extern mod portaudio;
 
 use portaudio::*;
 
 fn main() -> () {
-    io::println(fmt!("Portaudio version : %d", pa::get_version() as int));
-    io::println(fmt!("Portaudio version text : %s", pa::get_version_text()));
-    io::println(fmt!("Portaudio error text : %s", pa::get_error_text(types::PaNotInitialized)));
+    println!("Portaudio version : {}", pa::get_version());
+    println!("Portaudio version text : {}", pa::get_version_text());
+    println!("Portaudio error text : {}", pa::get_error_text(types::PaNotInitialized));
 
-    io::println(fmt!("Portaudio init error : %s", pa::get_error_text(pa::initialize())));
+    println!("Portaudio init error : {}", pa::get_error_text(pa::initialize()));
     
     let host_count = pa::get_host_api_count();
-    io::println(fmt!("Portaudio host count : %d", host_count as int));
+    println!("Portaudio host count : {}", host_count as int);
 
     let default_host = pa::get_default_host_api();
-    io::println(fmt!("Portaudio default host : %d", default_host as int));
+    println!("Portaudio default host : {}", default_host as int);
 
     let host_info = pa::get_host_api_info(default_host);
-    io::println(fmt!("Portaudio host name : %s", host_info.unwrap().name));
+    println!("Portaudio host name : {}", host_info.unwrap().name);
 
-    io::println(fmt!("Portaudio type id : %d",
-                    pa::host_api_type_id_to_host_api_index(types::PaCoreAudio) as int));
+    println!("Portaudio type id : {}",
+             pa::host_api_type_id_to_host_api_index(types::PaCoreAudio) as int);
 
     let def_input = pa::get_default_input_device();
     let info_input = pa::get_device_info(def_input).unwrap();
-    io::println("Default input device info :");
-    io::println(fmt!("version : %d", info_input.struct_version));
-    io::println(fmt!("name : %s", info_input.name));
-    io::println(fmt!("max input channels : %d", info_input.max_input_channels));
-    io::println(fmt!("max output channels : %d", info_input.max_output_channels));
-    io::println(fmt!("default sample rate : %f", info_input.default_sample_rate as float));
+    println!("Default input device info :");
+    println!("version : {}", info_input.struct_version);
+    println!("name : {}", info_input.name);
+    println!("max input channels : {}", info_input.max_input_channels);
+    println!("max output channels : {}", info_input.max_output_channels);
+    println!("default sample rate : {}", info_input.default_sample_rate);
 
 
     if pa::get_device_info(def_input).is_none() {
-        io::println("BOULET");
+       println!("error");
     }
     // PaStream test :
     let stream_params  = types::PaStreamParameters {
@@ -45,7 +45,7 @@ fn main() -> () {
     };
 
     let def_output = pa::get_default_output_device();
-    io::println(fmt!("name : %s", pa::get_device_info(def_output).unwrap().name));
+    println!("name : {}", pa::get_device_info(def_output).unwrap().name);
 
     let stream_params_out = types::PaStreamParameters {
         device : def_output,
@@ -55,16 +55,16 @@ fn main() -> () {
     };
 
 
-    let mut stream : pa::PaStream<f32, f32> = pa::PaStream::new(types::PaFloat32);
+    let mut stream : pa::PaStream<f32> = pa::PaStream::new(types::PaFloat32);
 
     let mut err= stream.open(Some(&stream_params), Some(&stream_params_out), 44100., 1024, types::PaClipOff);
 
-    io::println(fmt!("Portaudio Open error : %s", pa::get_error_text(err)));
+    println!("Portaudio Open error : {}", pa::get_error_text(err));
 
-    io::println(fmt!("Stream is active : %s", pa::get_error_text(stream.is_active())));
+    //println!("Stream is active : {}", pa::get_error_text(stream.is_active().unwrap()));
 
     err = stream.start();
-    io::println(fmt!("Portaudio Start error : %s", pa::get_error_text(err)));
+    println!("Portaudio Start error : {}", pa::get_error_text(err));
 
     let mut test;
     loop {
@@ -72,7 +72,7 @@ fn main() -> () {
         while test == 0 {
             test = stream.get_stream_write_available();
         }
-            io::println(fmt!("Stream Write available : %d", test as int)); 
+            println!("Stream Write available : {}", test as int); 
 
         match stream.read(1024) {
             Ok(res)     => {
@@ -81,17 +81,17 @@ fn main() -> () {
                 // }
                 stream.write(res, 1024)
             },
-            Err(err)    => fail!(fmt!("Portaudio error read : %s", pa::get_error_text(err)))
+            Err(err)    => fail!(format!("Portaudio error read : {}", pa::get_error_text(err)))
         };
     }
 
     err = types::PaNotInitialized;
 
     err = stream.close();
-    io::println(fmt!("Portaudio Close stream error : %s", pa::get_error_text(err)));
+    println!("Portaudio Close stream error : {}", pa::get_error_text(err));
 
-    io::println("");
+    println!("");
 
 
-    io::println(fmt!("Portaudio terminate error : %s", pa::get_error_text(pa::terminate())));
+    println!("Portaudio terminate error : {}", pa::get_error_text(pa::terminate()));
 }
