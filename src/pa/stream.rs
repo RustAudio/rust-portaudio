7b3717fa4fa334f7c8584bca7e8043d6fec6fa57
mitchmindtree <mitchell.nordine@gmail.com>
2015-12-23 17:05:02 +1100

    Remove unnecessary import

--- src/pa/stream.rs
+++ src/pa/stream.rs
@@ -17,7 +17,6 @@ use super::types::{
     SampleFormat,
     StreamAvailable,
     StreamCallbackFlags,
-    StreamCallbackFn,
     StreamCallbackTimeInfo,
     StreamCallbackResult,
     StreamFlags,
