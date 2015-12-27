e6455c6012ba8c8c2bbf42cbdfc98936c2aa937f
mitchmindtree <mitchell.nordine@gmail.com>
2015-12-27 16:25:59 +1100

    Move device and host APIs into PortAudio type API ensuring greater type safety

--- src/pa/mod.rs
+++ src/pa/mod.rs
@@ -26,7 +26,26 @@ use libc::c_double;
 use std::ptr;
 
 pub use self::error::Error;
-pub use self::stream::Stream;
+pub use self::stream::{
+    Available as StreamAvailable,
+    Blocking,
+    callback_flags as stream_callback_flags,
+    CallbackFlags as StreamCallbackFlags,
+    CallbackResult as StreamCallbackResult,
+    CallbackTimeInfo as StreamCallbackTimeInfo,
+    DuplexSettings as DuplexStreamSettings,
+    DuplexCallbackArgs as DuplexStreamCallbackArgs,
+    flags as stream_flags,
+    Flags as StreamFlags,
+    Flow,
+    Info as StreamInfo,
+    InputSettings as InputStreamSettings,
+    InputCallbackArgs as InputStreamCallbackArgs,
+    NonBlocking,
+    Parameters as StreamParameters,
+    Settings as StreamSettings,
+    Stream,
+};
 pub use self::types::{
     DeviceIndex,
     DeviceInfo,
@@ -36,24 +55,11 @@ pub use self::types::{
     HostApiTypeId,
     HostErrorInfo,
     SampleFormat,
-    StreamAvailable,
-    stream_callback_flags,
-    StreamCallbackFlags,
-    StreamCallbackTimeInfo,
-    StreamCallbackResult,
-    stream_flags,
-    StreamFlags,
-    StreamInfo,
-    StreamParameters,
     Time,
     FRAMES_PER_BUFFER_UNSPECIFIED,
-    NO_DEVICE,
-    USE_HOST_API_SPECIFIC_DEVICE_SPECIFICATION,
 };
 
 pub mod error;
-pub mod device;
-pub mod host;
 pub mod stream;
 mod types;
 
@@ -116,6 +122,223 @@ impl PortAudio {
         version_text()
     }
 
+    /// Retrieve the number of available devices. The number of available devices may be zero.
+    ///
+    /// **Note:** The only error documented by PortAudio that may occur calling this method is
+    /// caused by PortAudio not being initialised, however this should not be possible with our
+    /// type-safe **PortAudio** API.
+    pub fn device_count(&self) -> DeviceIndex {
+        unsafe {
+            ffi::Pa_GetDeviceCount().into()
+        }
+    }
+
+    /// Retrieve the index of the default input device. The result can be used in the
+    /// **InSettings** used to open an **In** **Stream**.
+    ///
+    /// Returns the default output device index for the default host API.
+    ///
+    /// Returns `None` if no default input device is available or an error was encountered.
+    pub fn default_input_device(&self) -> DeviceIndex {
+        unsafe {
+            ffi::Pa_GetDefaultInputDevice().into()
+        }
+    }
+
+    /// Retrieve the index of the default output device. The result can be used in the
+    /// **OutSettings** used to open an **Out** **Stream**.
+    ///
+    /// Returns the default input device index for the default host API.
+    ///
+    /// Returns `None` if no default input device is available or an error was encountered.
+    pub fn default_output_device(&self) -> DeviceIndex {
+        unsafe {
+            ffi::Pa_GetDefaultOutputDevice().into()
+        }
+    }
+
+    /// Retrieve a **DeviceInfo** structure containing information about the specified device.
+    ///
+    /// Returns Ok(DeviceInfo) if successful.
+    ///
+    /// Returns Err(Error::InvalidDevice) if the device parameter is out of range.
+    ///
+    /// # Arguments
+    ///
+    /// - device - A valid device index in the range 0 to (port_audio.device_count()-1).
+    pub fn device_info(&self, device: DeviceIndex) -> Result<DeviceInfo, Error> {
+        let c_info = unsafe { ffi::Pa_GetDeviceInfo(device.into()) };
+        if c_info.is_null() {
+            Err(Error::InvalidDevice)
+        }
+        else {
+            Ok(DeviceInfo::wrap(c_info))
+        }
+    }
+
+    /// Retrieve the number of available host APIs.
+    ///
+    /// Even if a host API is available it may have no devices available.
+    ///
+    /// Return a non-negative value indicating the number of available host APIs or an `Error` if
+    /// an error is encountered.
+    ///
+    /// TODO: Determine exactly what errors might occur (PA docs aren't clear on this).
+    pub fn host_api_count(&self) -> Result<HostApiIndex, Error> {
+        unsafe {
+            result_from_host_api_index(ffi::Pa_GetHostApiCount())
+        }
+    }
+
+    /// Retrieve the index of the default host API.
+    ///
+    /// The default host API will be the lowest common denominator host API on the current platform
+    /// and is unlikely to provide the best performance.
+    ///
+    /// Return a non-negative value ranging from 0 to (get_host_api_count()-1) indicating the
+    /// default host API index or an `Error` if an error is encountered.
+    ///
+    /// TODO: Determine exactly what errors might occur (PA docs aren't clear on this).
+    pub fn default_host_api(&self) -> Result<HostApiIndex, Error> {
+        unsafe {
+            result_from_host_api_index(ffi::Pa_GetDefaultHostApi())
+        }
+    }
+
+    /// Retrieve a pointer to a structure containing information about a specific host Api.
+    ///
+    /// # Arguments
+    ///
+    /// - host_api - A valid host API index ranging from 0 to (Pa_GetHostApiCount()-1)
+    ///
+    /// Return `Some(PaHostApiInfo)` describing a specific host API. If the `host_api` parameter is
+    /// out of range or an error is encountered, the function returns `None`.
+    pub fn host_api_info(&self, host_api: HostApiIndex) -> Option<HostApiInfo> {
+        let c_host_info = unsafe { ffi::Pa_GetHostApiInfo(host_api as ffi::HostApiIndex) };
+        if c_host_info.is_null() {
+            None
+        } else {
+            Some(HostApiInfo::wrap(c_host_info))
+        }
+    }
+
+    /// Convert a static host API unique identifier, into a runtime host API index.
+    ///
+    /// # Arguments
+    ///
+    /// - typde_id - A unique host API identifier belonging to the `PaHostApiTypeId` enumeration.
+    ///
+    /// Return a valid `HostApiIndex` ranging from 0 to (get_host_api_count()-1) or an `Error` if
+    /// an error is encountered.
+    ///
+    /// TODO: Determine exactly what errors might occur (PA docs aren't clear on this).
+    pub fn host_api_type_id_to_host_api_index(type_id: HostApiTypeId) -> Result<HostApiIndex, Error> {
+        unsafe {
+            result_from_host_api_index(ffi::Pa_HostApiTypeIdToHostApiIndex(type_id as i32))
+        }
+    }
+
+    /// Convert a host-API-specific device index to standard PortAudio device index.
+    ///
+    /// This function may be used in conjunction with the `device_count` field of `HostApiInfo` to
+    /// enumerate all devices for the specified host API.
+    ///
+    /// # Arguments
+    ///
+    /// - `host_api` - A valid host API index ranging from 0 to (get_host_api_count()-1)
+    /// - `host_api_device_index` - A valid per-host device index in the range 0 to
+    /// (get_host_api_info(host_api).device_count-1)
+    ///
+    /// Return a non-negative `DeviceIndex` ranging from 0 to (get_device_count()-1)
+    /// or an `Error` if an error is encountered.
+    ///
+    /// TODO: Determine exactly what errors might occur (PA docs aren't clear on this).
+    pub fn api_device_index_to_device_index(host_api: HostApiIndex,
+                                            host_api_device_index: i32) -> DeviceIndex {
+        unsafe {
+            ffi::Pa_HostApiDeviceIndexToDeviceIndex(host_api as i32, host_api_device_index).into()
+        }
+    }
+
+    /// Determine whether it would be possible to open an input-only stream with the specified
+    /// parameters.
+    ///
+    /// The `suggested_latency` field of the `StreamParameters` is ignored.
+    ///
+    /// Returns `Ok(())` if the format is supported, and an `Err(Error)` indicating why the format
+    /// is not supported otherwise.
+    pub fn is_input_format_supported<I>(&self, params: StreamParameters<I>, sample_rate: f64)
+        -> Result<(), Error>
+        where I: Sample,
+    {
+        is_format_supported(Some(params.into()), None, sample_rate)
+    }
+
+    /// Determine whether it would be possible to open an output-only stream with the specified
+    /// parameters.
+    ///
+    /// The `suggested_latency` field of the `StreamParameters` is ignored.
+    ///
+    /// Returns `Ok(())` if the format is supported, and an `Err(Error)` indicating why the format
+    /// is not supported otherwise.
+    pub fn is_output_format_supported<O>(&self, params: StreamParameters<O>, sample_rate: f64)
+        -> Result<(), Error>
+        where O: Sample,
+    {
+        is_format_supported(None, Some(params.into()), sample_rate)
+    }
+
+    /// Determine whether it would be possible to open a duplex stream with the specified
+    /// parameters.
+    ///
+    /// The `suggested_latency` field of the `StreamParameters` is ignored.
+    ///
+    /// Returns `Ok(())` if the format is supported, and an `Err(Error)` indicating why the format
+    /// is not supported otherwise.
+    pub fn is_duplex_format_supported<I, O>(&self,
+                                            in_params: StreamParameters<I>,
+                                            out_params: StreamParameters<O>,
+                                            sample_rate: f64)
+        -> Result<(), Error>
+        where I: Sample,
+              O: Sample,
+    {
+        is_format_supported(Some(in_params.into()), Some(out_params.into()), sample_rate)
+    }
+
+    /// Spawn a new blocking [**Stream**](./stream/struct.Stream) with the given settings.
+    pub fn open_blocking_stream<'a, S>(&'a self, settings: S)
+        -> Result<Stream<'a, Blocking<<S::Flow as Flow>::Buffer>, S::Flow>, Error>
+        where S: StreamSettings,
+              S::Flow: Flow + 'a,
+    {
+        Stream::<'a, Blocking<<S::Flow as Flow>::Buffer>, S::Flow>::open(self, settings)
+    }
+
+    /// Spawn a new non-blocking [**Stream**](./stream/struct.Stream) with the given settings.
+    pub fn open_non_blocking_stream<'a, S, C>(&'a self, settings: S, callback: C)
+        -> Result<Stream<'a, NonBlocking, S::Flow>, Error>
+        where S: StreamSettings,
+              S::Flow: Flow + 'a,
+              C: FnMut(<S::Flow as Flow>::CallbackArgs) -> StreamCallbackResult + 'static,
+    {
+        Stream::<'a, NonBlocking, S::Flow>::open(self, settings, callback)
+    }
+
+    /// Put the caller to sleep for at least 'msec' milliseconds.
+    ///
+    /// In the original API this function is provided only as a convenience for authors of portable
+    /// code (such as the tests and examples in the PortAudio distribution). This may be removed in
+    /// the future in favour of rust's `::std::thread::sleep` function.
+    ///
+    /// The function may sleep longer than requested so don't rely on this for accurate musical
+    /// timing.
+    pub fn sleep(&self, m_sec: i32) -> () {
+        unsafe {
+            ffi::Pa_Sleep(m_sec)
+        }
+    }
+
     /// Return information about the last host error encountered.
     ///
     /// The error information returned by this method will never be modified asynchronously by
@@ -131,62 +354,6 @@ impl PortAudio {
     pub fn last_host_error_info(&self) -> HostErrorInfo {
         let c_error = unsafe { ffi::Pa_GetLastHostErrorInfo() };
         HostErrorInfo::wrap(c_error)
-    }
-
-    /// Determine whether it would be possible to open an input-only stream with the specified
-    /// parameters.
-    ///
-    /// The `suggested_latency` field of the `StreamParameters` is ignored.
-    ///
-    /// Returns `Ok(())` if the format is supported, and an `Err(Error)` indicating why the format
-    /// is not supported otherwise.
-    pub fn is_input_format_supported(&self, params: &StreamParameters, sample_rate: f64)
-        -> Result<(), Error>
-    {
-        is_format_supported(Some(params), None, sample_rate)
-    }
-
-    /// Determine whether it would be possible to open an output-only stream with the specified
-    /// parameters.
-    ///
-    /// The `suggested_latency` field of the `StreamParameters` is ignored.
-    ///
-    /// Returns `Ok(())` if the format is supported, and an `Err(Error)` indicating why the format
-    /// is not supported otherwise.
-    pub fn is_output_format_supported(&self, params: &StreamParameters, sample_rate: f64)
-        -> Result<(), Error>
-    {
-        is_format_supported(None, Some(params), sample_rate)
-    }
-
-    /// Determine whether it would be possible to open a duplex stream with the specified
-    /// parameters.
-    ///
-    /// The `suggested_latency` field of the `StreamParameters` is ignored.
-    ///
-    /// Returns `Ok(())` if the format is supported, and an `Err(Error)` indicating why the format
-    /// is not supported otherwise.
-    pub fn is_duplex_format_supported(&self,
-                                      in_params: &StreamParameters,
-                                      out_params: &StreamParameters,
-                                      sample_rate: f64)
-        -> Result<(), Error>
-    {
-        is_format_supported(Some(in_params), Some(out_params), sample_rate)
-    }
-
-    /// Put the caller to sleep for at least 'msec' milliseconds.
-    ///
-    /// In the original API this function is provided only as a convenience for authors of portable
-    /// code (such as the tests and examples in the PortAudio distribution). This may be removed in
-    /// the future in favour of rust's `::std::thread::sleep` function.
-    ///
-    /// The function may sleep longer than requested so don't rely on this for accurate musical
-    /// timing.
-    pub fn sleep(&self, m_sec: i32) -> () {
-        unsafe {
-            ffi::Pa_Sleep(m_sec)
-        }
     }
 
 }
@@ -262,14 +429,12 @@ fn terminate() -> Result<(), Error> {
 /// Return Ok(()) if the format is supported, and an Error indicating why the format is not
 /// supported otherwise. The constant PaFormatIsSupported is provided to compare with the return
 /// value for success.
-fn is_format_supported(maybe_input_parameters: Option<&StreamParameters>,
-                       maybe_output_parameters: Option<&StreamParameters>,
+fn is_format_supported(maybe_input_parameters: Option<ffi::C_PaStreamParameters>,
+                       maybe_output_parameters: Option<ffi::C_PaStreamParameters>,
                        sample_rate : f64) -> Result<(), Error>
 {
-    let c_input = maybe_input_parameters.map(StreamParameters::unwrap).as_ref()
-        .map(|input| input as *const _);
-    let c_output = maybe_output_parameters.map(StreamParameters::unwrap).as_ref()
-        .map(|output| output as *const _);
+    let c_input = maybe_input_parameters.as_ref().map(|input| input as *const _);
+    let c_output = maybe_output_parameters.as_ref().map(|output| output as *const _);
     if c_input.is_none() && c_output.is_none() {
         Err(Error::InvalidDevice)
     } else {
@@ -283,6 +448,15 @@ fn is_format_supported(maybe_input_parameters: Option<&StreamParameters>,
         }
     }
 }
+
+
+fn result_from_host_api_index(idx: ffi::HostApiIndex) -> Result<HostApiIndex, Error> {
+    match idx {
+        idx if idx >= 0 => Ok(idx as u16),
+        err => Err(::num::FromPrimitive::from_i32(err).unwrap()),
+    }
+}
+
 
 /// Retrieve the size of a given sample format in bytes.
 ///
@@ -354,32 +528,33 @@ mod private {
 }
 
 impl private::SamplePrivate for f32 {
-    fn to_sample_format() -> SampleFormat { SampleFormat::Float32 }
+    fn to_sample_format() -> SampleFormat { SampleFormat::F32 }
 }
 
 impl private::SamplePrivate for i32 {
-    fn to_sample_format() -> SampleFormat { SampleFormat::Int32 }
+    fn to_sample_format() -> SampleFormat { SampleFormat::I32 }
 }
 
-impl private::SamplePrivate for u8 {
-    fn to_sample_format() -> SampleFormat { SampleFormat::UInt8 }
+impl private::SamplePrivate for i16 {
+    fn to_sample_format() -> SampleFormat { SampleFormat::I16 }
 }
 
 impl private::SamplePrivate for i8 {
-    fn to_sample_format() -> SampleFormat { SampleFormat::Int8 }
+    fn to_sample_format() -> SampleFormat { SampleFormat::I8 }
+}
+
+impl private::SamplePrivate for u8 {
+    fn to_sample_format() -> SampleFormat { SampleFormat::U8 }
 }
 
 /// public trait to constraint pa::Stream for specific types
 pub trait Sample: private::SamplePrivate {
     /// Retrieve the SampleFormat variant associated with the type.
-    fn sample_format_for<S: Sample>() -> SampleFormat {
-        S::to_sample_format()
-    }
-    /// Retrieve the SampleFormat variant associated with the type.
-    fn sample_format(&self) -> SampleFormat { Self::to_sample_format() }
+    fn sample_format() -> SampleFormat { Self::to_sample_format() }
 }
 
 impl Sample for f32 {}
 impl Sample for i32 {}
+impl Sample for i16 {}
 impl Sample for i8 {}
 impl Sample for u8 {}
