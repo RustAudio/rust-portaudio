00fee86bbbacf6f8118d74615db1b05955f2e5ee
Paul Houghton <paulirotta@gmail.com>
2019-04-01 15:23:14 +0300

    Replace deprecated try!() with ?

--- examples/saw.rs
+++ examples/saw.rs
@@ -14,24 +14,26 @@ const FRAMES_PER_BUFFER: u32 = 64;
 
 fn main() {
     match run() {
-        Ok(_) => {},
+        Ok(_) => {}
         e => {
             eprintln!("Example failed with the following: {:?}", e);
         }
     }
 }
 
-
 fn run() -> Result<(), pa::Error> {
-
-    println!("PortAudio Test: output sawtooth wave. SR = {}, BufSize = {}", SAMPLE_RATE, FRAMES_PER_BUFFER);
+    println!(
+        "PortAudio Test: output sawtooth wave. SR = {}, BufSize = {}",
+        SAMPLE_RATE, FRAMES_PER_BUFFER
+    );
 
     let mut left_saw = 0.0;
     let mut right_saw = 0.0;
 
-    let pa = try!(pa::PortAudio::new());
+    let pa = pa::PortAudio::new()?;
 
-    let mut settings = try!(pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
+    let mut settings =
+        pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
     // we won't output out of range samples so don't bother clipping them.
     settings.flags = pa::stream_flags::CLIP_OFF;
 
@@ -42,25 +44,29 @@ fn run() -> Result<(), pa::Error> {
         let mut idx = 0;
         for _ in 0..frames {
             buffer[idx] = left_saw;
-            buffer[idx+1] = right_saw;
+            buffer[idx + 1] = right_saw;
             left_saw += 0.01;
-            if left_saw >= 1.0 { left_saw -= 2.0; }
+            if left_saw >= 1.0 {
+                left_saw -= 2.0;
+            }
             right_saw += 0.03;
-            if right_saw >= 1.0 { right_saw -= 2.0; }
+            if right_saw >= 1.0 {
+                right_saw -= 2.0;
+            }
             idx += 2;
         }
         pa::Continue
     };
 
-    let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
+    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
 
-    try!(stream.start());
+    stream.start()?;
 
     println!("Play for {} seconds.", NUM_SECONDS);
     pa.sleep(NUM_SECONDS * 1_000);
 
-    try!(stream.stop());
-    try!(stream.close());
+    stream.stop()?;
+    stream.close()?;
 
     println!("Test finished.");
 
--- examples/sine.rs
+++ examples/sine.rs
@@ -14,20 +14,20 @@ const SAMPLE_RATE: f64 = 44_100.0;
 const FRAMES_PER_BUFFER: u32 = 64;
 const TABLE_SIZE: usize = 200;
 
-
 fn main() {
     match run() {
-        Ok(_) => {},
+        Ok(_) => {}
         e => {
             eprintln!("Example failed with the following: {:?}", e);
         }
     }
 }
 
-
 fn run() -> Result<(), pa::Error> {
-
-    println!("PortAudio Test: output sine wave. SR = {}, BufSize = {}", SAMPLE_RATE, FRAMES_PER_BUFFER);
+    println!(
+        "PortAudio Test: output sine wave. SR = {}, BufSize = {}",
+        SAMPLE_RATE, FRAMES_PER_BUFFER
+    );
 
     // Initialise sinusoidal wavetable.
     let mut sine = [0.0; TABLE_SIZE];
@@ -37,9 +37,10 @@ fn run() -> Result<(), pa::Error> {
     let mut left_phase = 0;
     let mut right_phase = 0;
 
-    let pa = try!(pa::PortAudio::new());
+    let pa = pa::PortAudio::new()?;
 
-    let mut settings = try!(pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
+    let mut settings =
+        pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
     // we won't output out of range samples so don't bother clipping them.
     settings.flags = pa::stream_flags::CLIP_OFF;
 
@@ -49,26 +50,30 @@ fn run() -> Result<(), pa::Error> {
     let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
         let mut idx = 0;
         for _ in 0..frames {
-            buffer[idx]   = sine[left_phase];
-            buffer[idx+1] = sine[right_phase];
+            buffer[idx] = sine[left_phase];
+            buffer[idx + 1] = sine[right_phase];
             left_phase += 1;
-            if left_phase >= TABLE_SIZE { left_phase -= TABLE_SIZE; }
+            if left_phase >= TABLE_SIZE {
+                left_phase -= TABLE_SIZE;
+            }
             right_phase += 3;
-            if right_phase >= TABLE_SIZE { right_phase -= TABLE_SIZE; }
+            if right_phase >= TABLE_SIZE {
+                right_phase -= TABLE_SIZE;
+            }
             idx += 2;
         }
         pa::Continue
     };
 
-    let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
+    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
 
-    try!(stream.start());
+    stream.start()?;
 
     println!("Play for {} seconds.", NUM_SECONDS);
     pa.sleep(NUM_SECONDS * 1_000);
 
-    try!(stream.stop());
-    try!(stream.close());
+    stream.stop()?;
+    stream.close()?;
 
     println!("Test finished.");
 
