9b7dfad9f15cf136c7e739f021f194663cabc680
Kevin Ji <kevin@kevinji.com>
2021-01-04 20:43:51 -0800

    Remove no-longer relevant FIXME (#183)

--- src/lib.rs
+++ src/lib.rs
@@ -612,8 +612,6 @@ pub fn version() -> i32 {
 }
 
 /// Retrieve a textual description of the current PortAudio build.
-///
-/// FIXME: This should return a `&'static str`, not a `String`.
 pub fn version_text() -> Result<&'static str, ::std::str::Utf8Error> {
     unsafe { ffi::c_str_to_str(ffi::Pa_GetVersionText()) }
 }
